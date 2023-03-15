6a34d5041895f1c9408067f887537549d13ea0fe
carolinesougon <114413234+carolinesougon@users.noreply.github.com>
2023-03-15 18:06:09 -0300

    atualização

--- js/main.js
+++ js/main.js
@@ -3,5 +3,13 @@ const form = document.getElementById("novoItem")
 form.addEventListener("submit", (evento)=>{
     evento.preventDefault()
 
+    criarElemento(evento.target.elements['nome'].value,evento.target.elements['quantidade'].value)
 
 })
+
+function criarElemento(nome, quantidade){
+    console.log (nome)
+    console.log (quantidade)
+
+    document.createElement('li')
+}
