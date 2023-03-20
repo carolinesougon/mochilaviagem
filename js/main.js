b6cdd14f8006e173e3f59c8cfeb94fef2980fb1e
carolinesougon <114413234+carolinesougon@users.noreply.github.com>
2023-03-20 18:01:56 -0300

    atualização

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
 
-form.addEventListener("submit", (evento)=>{
+form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criarElemento(evento.target.elements['nome'].value,evento.target.elements['quantidade'].value)
-
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
 
-function criarElemento(nome, quantidade){
-    console.log (nome)
-    console.log (quantidade)
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
 
-    document.createElement('li')
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
 }
